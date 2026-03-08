773a5e37d43263b750d0522f9ff9b51a645f8ff7
Nickyyy <phamquocbao2401@gmail.com>
2026-03-09 02:47:56 +0700

    Factory contract for creating pairs

---
+++ contracts/Factory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./Pair.sol";
+
+contract Factory {
+
+    address[] public pairs;
+
+    function createPair() public returns (address) {
+
+        Pair pair = new Pair();
+
+        pairs.push(address(pair));
+
+        return address(pair);
+    }
+
+}
